0b2b179d7d68a120b2db8faf348e8b246df1c3a8
syntaxdesigner01 <syntaxdesigners@gmail.com>
2024-06-24 22:20:35 -0700

    feature: created infoBoxes and infobox component to wrap and display info

---
+++ app/Components/InfoBoxes.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import InfoBox from "./InfoBox";
+
+export default function InfoBoxes() {
+  return (
+    <section>
+      <div className="container-xl lg:container m-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+          <InfoBox
+            heading={"For Renters"}
+            backgroundColor="bg-gray-100"
+            buttonInfo={{
+              text: "Browse Properties",
+              link: "/properties",
+              backgroundColor: "bg-black",
+            }}
+          >
+            <span>
+              Find your dream rental property. Bookmark properties and contact
+              owner
+            </span>
+          </InfoBox>
+
+          <InfoBox
+            heading={"For Property Owners"}
+            backgroundColor="bg-blue-100"
+            buttonInfo={{
+              text: " Add Property",
+              link: "/properties/add",
+              backgroundColor: "bg-blue-500",
+            }}
+          >
+            <span>
+              List your properties and reach potential tenants. Rent as an
+              airbnb or long term.
+            </span>
+          </InfoBox>
+        </div>
+      </div>
+    </section>
+  );
+}
