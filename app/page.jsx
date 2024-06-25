22c59b1c6359333ce0a39926f157f0425e709224
syntaxdesigner01 <syntaxdesigners@gmail.com>
2024-06-25 01:17:18 -0700

    feature: worked on page not found page

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Link from "next/link";
 import Hero from "./Components/Hero";
 import InfoBoxes from "./Components/InfoBoxes";
+import HomeProperties from "./Components/HomeProperties";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 }
